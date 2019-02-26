2c2ed1faf08779910fc05c90c6e74dced9ebe5f9
Pavlo Zahozhenko <pavlozahozhenko@gmail.com>
2019-02-27 01:05:01 +0200

    Passing params in ruby blocks; procs

--- ruby_basics/blocks.rb
+++ ruby_basics/blocks.rb
@@ -1,7 +1,11 @@
 class A
-  def method_with_block(arg)
+  def method_with_block(arg, my_proc, &block)
     puts 'method begins'
-    yield if block_given?
-    puts 'method ends'
+    v = arg.to_i * 2
+    puts "Block class: #{block.class}"
+    return_val = block.call(v)
+    return_val = my_proc.call(return_val)
+    #yield(v) if block_given?
+    puts "method ends #{return_val}"
   end
 end
