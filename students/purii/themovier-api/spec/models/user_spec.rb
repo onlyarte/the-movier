0e596387d3c87485b6e5a8b214f24e3af56778ef
Ruslan Purii <ruslanpurii@gmail.com>
2019-04-01 13:56:23 +0300

    User lists spec

--- students/purii/themovier-api/spec/models/user_spec.rb
+++ students/purii/themovier-api/spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # ensure one user has many follows
-  # it { should have_many(:follows).dependent(:destroy) }
-
   # ensure one user follows many users
   it { should have_many(:followings) }
 
   # ensure one user has many followers
   it { should have_many(:followers) }
+
+  # ensure one user has many lists
+  it { should have_many(:lists) }
 
   # ensure columns email, password, username, name and is_super are present before saving
   it { should validate_presence_of(:email) }
