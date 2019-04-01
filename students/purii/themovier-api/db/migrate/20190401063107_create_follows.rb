aeca53987efbab8776ae8544e46cdb76294ab3b4
Ruslan Purii <ruslanpurii@gmail.com>
2019-04-01 11:50:16 +0300

    Refactored CreateFollow migration

--- students/purii/themovier-api/db/migrate/20190401063107_create_follows.rb
+++ students/purii/themovier-api/db/migrate/20190401063107_create_follows.rb
@@ -1,8 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[5.2]
   def change
     create_table :follows do |t|
-      t.references :follower, index: true, foreign_key: { to_table: :users }
       t.references :following, index: true, foreign_key: { to_table: :users }
+      t.references :followed, index: true, foreign_key: { to_table: :users }
       t.timestamp :created_at
 
       t.timestamps
